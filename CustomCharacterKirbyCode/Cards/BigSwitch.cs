f0bcc40e2564210d3741ba3e4cd4d94285486051
KindAiden <aidenwedema@gmail.com>
2026-04-21 09:32:03 +0200

    new cards

---
+++ CustomCharacterKirbyCode/Cards/BigSwitch.cs
@@ -0,0 +1,29 @@
+﻿using CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
+
+public class BigSwitch() : CustomCharacterKirbyCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        BigSwitch card = this;
+        
+        CardModel? choice = CardFactory.GetDistinctForCombat(card.Owner, card.Owner.Character.CardPool.GetUnlockedCards(card.Owner.UnlockState, card.Owner.RunState.CardMultiplayerConstraint).Where(c => c.Rarity == CardRarity.Uncommon), 1, card.Owner.RunState.Rng.CombatCardGeneration).FirstOrDefault();
+        if (choice == null)
+            return;
+        choice.SetToFreeThisTurn();
+        await CardPileCmd.AddGeneratedCardToCombat(choice, PileType.Hand, true);
+    }
+
+    protected override void OnUpgrade() => RemoveKeyword(CardKeyword.Exhaust);
+}
---
+++ CustomCharacterKirbyCode/Cards/PrismShield.cs
@@ -0,0 +1,26 @@
+﻿using CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
+
+public class PrismShield() : CustomCharacterKirbyCard(2, CardType.Power, CardRarity.Common, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(5M, ValueProp.Move)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<PlatingPower>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        PrismShield card = this;
+
+        await PowerCmd.Apply<PlatingPower>(card.Owner.Creature, DynamicVars.Block.BaseValue, card.Owner.Creature, card);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Block.UpgradeValueBy(2M);
+}
