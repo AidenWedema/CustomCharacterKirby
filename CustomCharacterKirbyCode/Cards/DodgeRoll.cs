2c55f56616ed3c9e402735e75552af3db43ee3c3
KindAiden <aidenwedema@gmail.com>
2026-04-28 13:26:03 +0200

    Add new card and associated power

---
+++ CustomCharacterKirbyCode/Cards/DodgeRoll.cs
@@ -0,0 +1,26 @@
+﻿using CustomCharacterKirby.CustomCharacterKirbyCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
+
+public class DodgeRoll() : CustomCharacterKirbyCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(0M, ValueProp.Move)];
+
+    public override bool GainsBlock => IsUpgraded;
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        DodgeRoll card = this;
+        await PowerCmd.Apply<DodgeRollPower>(card.Owner.Creature, 1, card.Owner.Creature, card);
+        
+        if(DynamicVars.Block.BaseValue > 0)
+            await CreatureCmd.GainBlock(card.Owner.Creature, DynamicVars.Block, cardPlay);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.Block.UpgradeValueBy(5M);
+}
---
+++ CustomCharacterKirbyCode/Powers/DodgeRollPower.cs
@@ -0,0 +1,23 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace CustomCharacterKirby.CustomCharacterKirbyCode.Powers;
+
+public class DodgeRollPower : CustomCharacterKirbyPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Single;
+    
+    public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props, Creature? dealer, CardModel? cardSource)
+    {
+        DodgeRollPower power = this;
+        if (target != power.Owner || result.UnblockedDamage > 0) return;
+        power.Flash();
+        await PowerCmd.Apply<EnergyNextTurnPower>(power.Owner, 1, power.Owner, null);
+    }
+}
