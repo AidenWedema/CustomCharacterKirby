04ef81209d66d13d5bbecb08d5e36d58dda9fa0b
KindAiden <aidenwedema@gmail.com>
2026-06-04 09:29:50 +0200

    Changed Crackler and Prism Shield to be uncommon and slightly tweaked values to fit that rarity better.

--- CustomCharacterKirbyCode/Cards/Crackler.cs
+++ CustomCharacterKirbyCode/Cards/Crackler.cs
@@ -1,17 +1,15 @@
-﻿using CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
-using CustomCharacterKirby.CustomCharacterKirbyCode.Powers;
+﻿using CustomCharacterKirby.CustomCharacterKirbyCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.ValueProps;
 
 namespace CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
 
-public class Crackler() : CustomCharacterKirbyCard(1, CardType.Power, CardRarity.Common, TargetType.Self)
+public class Crackler() : CustomCharacterKirbyCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DynamicVar("CracklerDamage", 4M), new DynamicVar("TurnAmount", 6)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DynamicVar("CracklerDamage", 6M), new DynamicVar("TurnAmount", 6)];
 
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<CracklerPower>()];
     
--- CustomCharacterKirbyCode/Cards/PrismShield.cs
+++ CustomCharacterKirbyCode/Cards/PrismShield.cs
@@ -1,16 +1,14 @@
 ﻿using BaseLib.Extensions;
-using CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
-using MegaCrit.Sts2.Core.ValueProps;
 
 namespace CustomCharacterKirby.CustomCharacterKirbyCode.Cards;
 
-public class PrismShield() : CustomCharacterKirbyCard(2, CardType.Power, CardRarity.Common, TargetType.Self)
+public class PrismShield() : CustomCharacterKirbyCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<PlatingPower>(5M)];
 
